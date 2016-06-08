1e9ae9e4d58c41f6a97ffcd9376109fcbb324f17
ascoelho <anthonystefancoelho@gmail.com>
2016-06-08 10:29:01 -0400

    first commit

---
+++ Quote Pro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quote Pro
+//
+//  Created by Anthony Coelho on 2016-06-08.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
